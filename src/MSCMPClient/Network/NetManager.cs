d185013f612c584f0c27145bfdfb23d2657f3732
CurtisVL <voxelloop@gmail.com>
2019-05-09 23:06:04 +0100

    Fix players joining each other with different client versions

--- src/MSCMPClient/Network/NetManager.cs
+++ src/MSCMPClient/Network/NetManager.cs
@@ -162,7 +162,7 @@ namespace MSCMP.Network {
 			// Hopefully this will fix people playing with different mod versions!
 			string versionFull = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
 			int buildNumber = Convert.ToInt32(versionFull.Substring(versionFull.LastIndexOf('.') + 1));
-			PROTOCOL_VERSION = 1;
+			PROTOCOL_VERSION = buildNumber;
 
 			p2pSessionRequestCallback = Steamworks.Callback<Steamworks.P2PSessionRequest_t>.Create(OnP2PSessionRequest);
 			p2pConnectFailCallback = Steamworks.Callback<Steamworks.P2PSessionConnectFail_t>.Create(OnP2PConnectFail);
